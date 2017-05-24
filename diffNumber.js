e37ef8628350969fbfedeae10a55fdb9712d504c
freda nada <fredasnada@gmail.com>
2017-05-24 15:04:16 -0400

    add diffNumber solution

--- diffNumber.js
+++ diffNumber.js
@@ -1,3 +1,13 @@
 /*
 Given an array arr of unique nonnegative integers, implement a function diffNumber that finds an integer that is NOT in the array.
+const array1 = [59, 2, 0] => 1
+const array2 = [0,1,2,3,4] => 5
 */
+
+function diffNumber (arr) {
+  let nums = new Set(arr)
+  for(let i=0;i<nums.size;i++){
+    if(!nums.has(i)) return i
+  }
+  return nums.size //when the input is in order from 0
+}
