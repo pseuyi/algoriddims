c9566a26f430787f635e55c2ee6b79d4b13b599b
freda nada <fredasnada@gmail.com>
2017-06-20 12:05:24 -0400

    notes

--- pramp/coinChange.js
+++ pramp/coinChange.js
@@ -2,11 +2,19 @@ function coinChange (money, denom) {
   const memo = []
   const ways = []
   denom.forEach(coin=>memo.push([coin]))
+
   memo.forEach(way=>{
     let waySum = way.reduce((a,b)=>a+b)
+    console.log(waySum)
     if(waySum===money) ways.push(way)
+    else if (waySum < money) // ???
+
   })
+
   return ways
 }
 
 console.log(coinChange(4, [1,2,3]))
+
+// powerSet of ways
+// permutations of ways
