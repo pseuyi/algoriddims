dc7691c85f150ead1b8d854dfa45662c76aaf454
freda nada <fredasnada@gmail.com>
2017-06-21 15:15:34 -0400

    generate fibonacci sequence solution

--- fibonacci.js
+++ fibonacci.js
@@ -7,5 +7,24 @@ function fibonacci (n) {
   return fibonacci(n-1) + fibonacci(n-2)
 }
 
-console.log(fibonacci(5))
+// console.log(fibonacci(5))
 // 1 1 2 3 5
+
+/*
+generate a fibonacci sequence of n length
+*/
+
+function fibSequence (n) {
+  const seq = []
+  let i = 0, p, q
+
+  while (i<n) {
+    p = i-1 < 0 ? 1 : seq[i-1]
+    q = i-2 < 0 ? 0 : seq[i-2]
+    seq[i] = p + q
+    console.log(seq[i])
+    i++
+  }
+}
+
+// fibSequence(5)
