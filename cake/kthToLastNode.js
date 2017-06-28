87b5d25418c149dda49e6bac5b0aa562be53f1c4
freda nada <fredasnada@gmail.com>
2017-06-27 21:44:24 -0400

    init

---
+++ cake/kthToLastNode.js
@@ -0,0 +1,23 @@
+/*
+find the kth to last node, given an integer k and a head node
+
+function LinkedListNode(value) {
+  this.value = value;
+  this.next = null;
+}
+
+var a = new LinkedListNode("Angel Food");
+var b = new LinkedListNode("Bundt");
+var c = new LinkedListNode("Cheese");
+var d = new LinkedListNode("Devil's Food");
+var e = new LinkedListNode("Eccles");
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+kthToLastNode(2, a);
+// returns the node with value "Devil's Food" (the 2nd to last node)
+
+*/
