a1edf3fd7fff70a9b5285a11bd4cbb6953e2d48d
freda nada <fredasnada@gmail.com>
2017-06-28 11:27:57 -0400

    refactor

--- cake/kthToLastNode.js
+++ cake/kthToLastNode.js
@@ -19,43 +19,42 @@ c.next = d;
 d.next = e;
 
 //working solution
-function kthToLastNode (k, node) {
-  let size = 0, curr = node, i = 0
+// function kthToLastNode (k, node) {
+//   let size = 0, curr = node, i = 0
+//
+//   while (curr) {
+//     curr = curr.next
+//     size++
+//   }
+//
+//   curr = node
+//   while (i<size-k) {
+//     curr = curr.next
+//     i++
+//   }
+//
+//   return curr
+// }
 
-  while (curr) {
-    curr = curr.next
-    size++
+//runner solution
+
+function kthToLastNode (k, node) {
+  let curr = node, i = 0
+  let last
+
+  while (i<k) {
+    last = curr.next
+    i++
   }
 
-  curr = node
-  while (i<size-k) {
+  while (last) {
     curr = curr.next
-    i++
+    last = last.next
   }
 
   return curr
 }
 
-//runner solution
-/*
-function kthToLastNode (k, node) {
-  let curr = node
-  let kthToLast
-  let i = 0
-
-  while (curr) {
-    kthToLast = curr
-    // traverse list in k steps
-    while(i<k) {
-      curr = curr.next || null
-      i++
-    }
-    i = 0
-  }
-
-  return kthToLast
-}
-*/
 
 
 console.log(kthToLastNode(2, a))
