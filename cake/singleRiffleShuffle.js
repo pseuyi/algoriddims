2553425526b9ed65b4e52a2c4d4c2ee8270c19b8
freda nada <fredasnada@gmail.com>
2017-06-30 18:29:49 -0400

    O(1) space

--- cake/singleRiffleShuffle.js
+++ cake/singleRiffleShuffle.js
@@ -6,8 +6,8 @@ const deck = new Array(52).fill().map((el,i)=>++i)
 
 function riffle (arr) {
   const mid = Math.floor(arr.length/2)
-  const half1 = arr.slice(0, mid)
-  const half2 = arr.slice(mid+1)
+  const half1 = arr.splice(0, mid)
+  const half2 = arr
   let shuffled = []
   while (shuffled.length <52 && half1.length && half2.length) {
     let i = Math.round(Math.random() * half1.length)
@@ -16,7 +16,6 @@ function riffle (arr) {
     let right = half2.splice(j)
     left.map(el=>shuffled.push(el))
     right.map(el=>shuffled.push(el))
-
   }
   if(half1.length) shuffled.concat(half1)
   if(half2.length) shuffled.concat(half2)
